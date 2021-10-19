93d61ccb9e7a01c8f36b0e176351410bc56ccc3d
TimMorrisDev <timmorris_16@hotmail.com>
2021-10-19 09:56:10 +0000

    Add get recipes function and display to recipes.html

--- app.py
+++ app.py
@@ -24,32 +24,38 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        # check if username already exists in db
-        existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
+@app.route("/get_recipes")
+def get_recipes():
+    recipes = list(mongo.db.recipes.find())
+    return render_template("recipes.html", recipes=recipes)
 
-        if existing_user:
-            flash("Username already exists")
-            return redirect(url_for("register"))
 
-        register = {
-            "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password")),
-            "email": request.form.get("email").lower(),
-            "user_recipes": [],
-            "saved_recipes": [],
-        }
-        mongo.db.users.insert_one(register)
+# @app.route("/register", methods=["GET", "POST"])
+# def register():
+#     if request.method == "POST":
+#         # check if username already exists in db
+#         existing_user = mongo.db.users.find_one(
+#             {"username": request.form.get("username").lower()})
 
-        # put the new user into 'session' cookie
-        session["user"] = request.form.get("username").lower()
-        flash("Registration Successful!")
-        return redirect(url_for("profile", username=session["user"]))
+#         if existing_user:
+#             flash("Username already exists")
+#             return redirect(url_for("register"))
 
-    return render_template("register.html")
+#         register = {
+#             "username": request.form.get("username").lower(),
+#             "password": generate_password_hash(request.form.get("password")),
+#             "email": request.form.get("email").lower(),
+#             "user_recipes": [],
+#             "saved_recipes": [],
+#         }
+#         mongo.db.users.insert_one(register)
+
+#         # put the new user into 'session' cookie
+#         session["user"] = request.form.get("username").lower()
+#         flash("Registration Successful!")
+#         return redirect(url_for("profile", username=session["user"]))
+
+#     return render_template("register.html")
 
 
 if __name__ == "__main__":
